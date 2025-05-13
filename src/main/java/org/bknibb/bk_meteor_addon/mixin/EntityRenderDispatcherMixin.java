8433f477df01cd1fd73f3d377050c555977b30c1
Bknibb <bailey.knibb@gmail.com>
2025-05-13 19:17:40 +1000

    release v1.4.2 Fixed MineplayRemoveOfflineRobloxPlayers remove player shadows breaking entity rendering

--- src/main/java/org/bknibb/bk_meteor_addon/mixin/EntityRenderDispatcherMixin.java
+++ src/main/java/org/bknibb/bk_meteor_addon/mixin/EntityRenderDispatcherMixin.java
@@ -8,21 +8,27 @@ import net.minecraft.client.render.entity.state.EntityRenderState;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.world.WorldView;
 import org.bknibb.bk_meteor_addon.MineplayUtils;
 import org.bknibb.bk_meteor_addon.modules.MineplayRemoveOfflineRobloxPlayers;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(EntityRenderDispatcher.class)
-public class EntityRenderDispatcherMixin {
-    @Inject(method = "render(Lnet/minecraft/entity/Entity;DDDFLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;ILnet/minecraft/client/render/entity/EntityRenderer;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/entity/EntityRenderDispatcher;renderShadow(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;Lnet/minecraft/client/render/entity/state/EntityRenderState;FFLnet/minecraft/world/WorldView;F)V"), cancellable = true)
-    private <E extends Entity, S extends EntityRenderState> void cancelShadowRendering(E entity, double x, double y, double z, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, EntityRenderer<? super E, S> renderer, CallbackInfo ci) {
+public abstract class EntityRenderDispatcherMixin {
+    @Shadow
+    protected static void renderShadow(MatrixStack matrices, VertexConsumerProvider vertexConsumers, EntityRenderState renderState, float opacity, float tickDelta, WorldView world, float radius) {}
+    @Redirect(method = "render(Lnet/minecraft/entity/Entity;DDDFLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;ILnet/minecraft/client/render/entity/EntityRenderer;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/entity/EntityRenderDispatcher;renderShadow(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;Lnet/minecraft/client/render/entity/state/EntityRenderState;FFLnet/minecraft/world/WorldView;F)V"))
+    private <E extends Entity> void cancelShadowRendering(MatrixStack matrices, VertexConsumerProvider vertexConsumers, EntityRenderState renderState, float opacity, float tickDelta, WorldView world, float radius, E entity) {
         if (entity instanceof PlayerEntity player) {
             if (MineplayUtils.hidePlayer(player) && Modules.get().get(MineplayRemoveOfflineRobloxPlayers.class).hidePlayerShadow.get()) {
-                ci.cancel();
+                return;
             }
         }
+        renderShadow(matrices, vertexConsumers, renderState, opacity, tickDelta, world, radius);
     }
 }
