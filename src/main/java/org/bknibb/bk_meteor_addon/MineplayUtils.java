91ee400928dbd9d47fe0b10d70da65679ba0e6b3
Bknibb <bailey.knibb@gmail.com>
2025-06-10 14:45:36 +1000

    remove old isDisconnectedPlayer functions

--- src/main/java/org/bknibb/bk_meteor_addon/MineplayUtils.java
+++ src/main/java/org/bknibb/bk_meteor_addon/MineplayUtils.java
@@ -1,40 +1,14 @@
 package org.bknibb.bk_meteor_addon;
 
-import meteordevelopment.meteorclient.systems.modules.Modules;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.PlayerListEntry;
-import net.minecraft.entity.player.PlayerEntity;
 
 public class MineplayUtils {
-public static boolean isRobloxPlayer(PlayerListEntry entry) {
+    public static boolean isRobloxPlayer(PlayerListEntry entry) {
         if (entry == null || entry.getDisplayName() == null || entry.getDisplayName().getStyle().getColor() == null) {
             return false;
         }
         return entry.getDisplayName().getStyle().getColor().getRgb() == 0xFF999B;
-    }
-    public static boolean isDisconnectedPlayer(PlayerEntity player) {
-        MinecraftClient client = MinecraftClient.getInstance();
-        if (client.getNetworkHandler() == null) return false;
-        PlayerListEntry entry = client.getNetworkHandler().getPlayerListEntry(player.getUuid());
-        if (entry == null) {
-            return false;
-        }
-        if (!isRobloxPlayer(entry)) return false;
-        return entry.getLatency() == 0;
-    }
-    public static boolean isDisconnectedPlayer(String player) {
-        MinecraftClient client = MinecraftClient.getInstance();
-        if (client.getNetworkHandler() == null) return false;
-        PlayerListEntry entry = client.getNetworkHandler().getPlayerListEntry(player);
-        if (entry == null) {
-            return false;
-        }
-        if (!isRobloxPlayer(entry)) return false;
-        return entry.getLatency() == 0;
-    }
-    public static boolean isDisconnectedPlayer(PlayerListEntry entry) {
-        if (!isRobloxPlayer(entry)) return false;
-        return entry.getLatency() == 0;
     }
     public static boolean isOnMineplay() {
         MinecraftClient client = MinecraftClient.getInstance();
@@ -44,6 +18,30 @@ public static boolean isRobloxPlayer(PlayerListEntry entry) {
         String serverName = client.getCurrentServerEntry().address;
         return serverName.contains("mineplay.nl");
     }
+//    public static boolean isDisconnectedPlayer(PlayerEntity player) {
+//        MinecraftClient client = MinecraftClient.getInstance();
+//        if (client.getNetworkHandler() == null) return false;
+//        PlayerListEntry entry = client.getNetworkHandler().getPlayerListEntry(player.getUuid());
+//        if (entry == null) {
+//            return false;
+//        }
+//        if (!isRobloxPlayer(entry)) return false;
+//        return entry.getLatency() == 0;
+//    }
+//    public static boolean isDisconnectedPlayer(String player) {
+//        MinecraftClient client = MinecraftClient.getInstance();
+//        if (client.getNetworkHandler() == null) return false;
+//        PlayerListEntry entry = client.getNetworkHandler().getPlayerListEntry(player);
+//        if (entry == null) {
+//            return false;
+//        }
+//        if (!isRobloxPlayer(entry)) return false;
+//        return entry.getLatency() == 0;
+//    }
+//    public static boolean isDisconnectedPlayer(PlayerListEntry entry) {
+//        if (!isRobloxPlayer(entry)) return false;
+//        return entry.getLatency() == 0;
+//    }
 //    public static boolean canHide() {
 //        return (isOnMineplay() && Modules.get().isActive(MineplayRemoveOfflineRobloxPlayers.class));
 //    }
